a14dd04651e4944ee222d136691ddb194c084386
BOTSPAM100 <52465433+BOTSPAM100@users.noreply.github.com>
2019-07-02 20:47:04 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -4,7 +4,7 @@ console.log("Scrpit By zYx");
 
 
 client.on("ready", () => {
-let channel =     client.channels.get("")
+let channel =     client.channels.get("566405981840605185")
 setInterval(function() {
 channel.send(`BOTSPAM - BY: ZYX ,, MR_ABOOODY ... SPAM,SPAM,SPAM,SPAM,SPAM`);
 }, 30)
